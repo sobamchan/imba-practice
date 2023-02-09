686e54d907849b766e8328a5da6d7e1544030de3
sobamchan <oh.sore.sore.soutarou@gmail.com>
2023-02-09 17:47:11 +0100

    correct basename

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { imba } from 'vite-plugin-imba';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	base: 'imba-practice',
+	base: '/imba-practice/',
 	plugins: [imba()],
 	build: { target: 'esnext' }
 });
